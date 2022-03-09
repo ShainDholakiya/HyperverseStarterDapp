3948fef9c536e789d509a869026020e02e92cb08
Shain-D <shain@synthesis.is>
2022-03-09 07:50:26 -0800

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { initialize, Provider } from "@decentology/hyperverse";
+import { networks } from "@decentology/hyperverse";
+import { Ethereum } from "@decentology/hyperverse-ethereum";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const hyperverse = initialize({
+    blockchain: Ethereum,
+    network: networks.Testnet,
+    modules: [],
+  });
+  return (
+    <Provider initialState={hyperverse}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
