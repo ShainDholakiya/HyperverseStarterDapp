3e6291c9ccd3b36fa9d49beefd346bac7c1c40de
ShainDholakiya <shainshain99@gmail.com>
2022-03-14 07:56:16 -0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,6 @@ import type { AppProps } from "next/app";
 import { initialize, Provider } from "@decentology/hyperverse";
 import { networks } from "@decentology/hyperverse";
 import { Ethereum } from "@decentology/hyperverse-ethereum";
-// import { Metis } from '@decentology/hyperverse-metis';
-// import { Flow } from '@decentology/hyperverse-flow';
-// import { Algorand } from '@decentology/hyperverse-algorand';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const hyperverse = initialize({
