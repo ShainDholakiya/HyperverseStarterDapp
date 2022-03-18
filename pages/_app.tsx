7fe588e2fbc7cfe235acf9bb7811f38133c344dc
shaindholakiya <shainshain99@gmail.com>
2022-03-18 10:00:51 -0700

    package updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { initialize, Provider } from "@decentology/hyperverse";
-import { networks } from "@decentology/hyperverse";
+import { initialize, Provider, Network } from "@decentology/hyperverse";
 import { Ethereum } from "@decentology/hyperverse-ethereum";
 // import { Metis } from '@decentology/hyperverse-metis';
 // import { Flow } from '@decentology/hyperverse-flow';
@@ -10,7 +9,7 @@ import { Ethereum } from "@decentology/hyperverse-ethereum";
 function MyApp({ Component, pageProps }: AppProps) {
   const hyperverse = initialize({
     blockchain: Ethereum,
-    network: networks.Testnet,
+    network: Network.Testnet,
     modules: [],
   });
   return (
